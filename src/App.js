09f85e50222682708fb0ea23b43e4ffdfcfeb19d
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-06-18 14:08:46 +0530

    implement uploadimage component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import UploadImage from "./components/uploadImage/uploadImage";
+import { observer } from "mobx-react-lite";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = observer(({ userStore }) => {
+  return (
+    <div className="App">
+      <UploadImage userStore={userStore} />
+    </div>
+  );
+});
 
 export default App;
