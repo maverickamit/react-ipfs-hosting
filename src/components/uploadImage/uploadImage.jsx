18cee30d9908ab302488fb0ee8a12079d76a9ada
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-06-23 18:19:39 +0530

    store/update error message on userstore for unsupported files

--- src/components/uploadImage/uploadImage.jsx
+++ src/components/uploadImage/uploadImage.jsx
@@ -6,51 +6,57 @@ import "@uppy/drag-drop/dist/style.css";
 import "./uploadImage.css";
 import { observer } from "mobx-react-lite";
 const UploadImage = observer(({ userStore }) => {
-  const uppy = new Uppy();
+  const uppy = new Uppy({
+    onBeforeFileAdded: (file, files) => {
+      userStore.setErrorMessage("");
+      userStore.setStorageValue("");
+      let reg = /(gif|jpg|jpeg|png|gif|svg|webp|pdf)$/i;
+      if (!reg.test(file.name)) {
+        userStore.setErrorMessage(
+          " Error: File type is not supported. Supported file types are JPG, JPEG, PNG, GIF, SVG, WEBP, PDF."
+        );
+      } else if (file.data.size >= 1048576) {
+        userStore.setErrorMessage(" Error: Max file size limit is 10 MB.");
+      } else {
+        const reader = new window.FileReader();
+        reader.readAsArrayBuffer(file.data);
+        reader.onloadend = () => {
+          setBuffer(Buffer(reader.result));
+        };
+      }
+    },
+  });
   const { create } = require("ipfs-http-client");
   const ipfs = create("https://ipfs.infura.io:5001");
   const [buffer, setBuffer] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async () => {
     console.log("Submitting the File...");
-    setIsLoading(true);
+    userStore.setIsLoading(true);
     const { cid } = await ipfs.add(buffer);
     console.log("IPFS Hash: ", cid.string);
     userStore.setStorageValue(cid.string);
-    setIsLoading(false);
+    userStore.setIsLoading(false);
   };
-  uppy.on("file-added", async (file) => {
-    const reader = new window.FileReader();
-    reader.readAsArrayBuffer(file.data);
-    reader.onloadend = () => {
-      setBuffer(Buffer(reader.result));
-    };
-  });
+
   useEffect(() => {
     if (buffer !== "") {
       handleSubmit();
     }
   }, [buffer]);
   return (
-    <div className="Container">
-      <nav className="navbar navbar-light bg-light">
-        <h1 className="navbar-brand mb-0 h1 text-center">IPFS Image Sharing</h1>
-      </nav>
-      <main role="main" className="inner cover mx-sm-3 mb-2 ">
-        <div className="drag-drop-btn">
-          <DragDrop
-            uppy={uppy}
-            locale={{
-              strings: {
-                dropHereOr: "Drop here or %{browse}",
-                browse: "browse",
-              },
-            }}
-          />
-          <p>{isLoading ? "Please wait.." : ""}</p>
-        </div>
-      </main>
+    <div>
+      <div className="drag-drop-btn">
+        <DragDrop
+          uppy={uppy}
+          locale={{
+            strings: {
+              dropHereOr: "Drop here or %{browse}",
+              browse: "browse",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 });
